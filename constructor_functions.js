f2d64487fc0f9b9073d069c2efbb949759a58d5b
CodingWithMK <032180115@ogr.uludag.edu.tr>
2023-10-01 18:34:43 +0300

    Dealing and learning how to use functions the right way in JavaScript

---
+++ constructor_functions.js
@@ -0,0 +1,10 @@
+// Constructor Functions
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1);

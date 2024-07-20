bdd1c8efa407024ef27e2acbadbc5f405b340cce
CodingWithMK <032180115@ogr.uludag.edu.tr>
2024-07-20 13:12:23 +0200

    JavaScript 'Hello World!' monitoring types.

---
+++ JavaScript101/lesson_1.js
@@ -0,0 +1,8 @@
+/* Lesson 1 - Hello World! */
+
+console.log("Hello World!") // Logs 'Hello World!' into the terminal
+
+alert("Hello") // Sends an alert message on the webpage
+
+document.write("Hello World!") // Overwrites the complete webpage and shows up 'Hello World!'
+
